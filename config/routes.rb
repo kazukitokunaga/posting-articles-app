fd89aa92bba59eca822756f70f0e4c4693cbc5db
kazukitokunaga <tokunagakazuki330@gmail.com>
2019-01-01 14:55:04 +0900

    モデル名変更（model→user） #4

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get 'page/home'
-  devise_for :models
+  devise_for module: :users, controllers: {
+    sessions:      'users/sessions',
+	  passwords:     'users/passwords',
+    registrations: 'users/registrations',
+    confirmation: 'users/confirmations',
+  }
+  root to: 'page#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_01_044203) do
+ActiveRecord::Schema.define(version: 2019_01_01_053400) do
 
-  create_table "models", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -32,10 +32,10 @@ ActiveRecord::Schema.define(version: 2019_01_01_044203) do
     t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["confirmation_token"], name: "index_models_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_models_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
-    t.index ["unlock_token"], name: "index_models_on_unlock_token", unique: true
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
 end
