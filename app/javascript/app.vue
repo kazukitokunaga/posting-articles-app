8db61e42e46d46185c02ee2bd2ce168496e455e8
kazukitokunaga <tokunagakazuki330@gmail.com>
2019-01-01 20:50:19 +0900

    CSRFトークンの設定（axiosの際に必要） #5

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -6,6 +6,11 @@
 </template>
 
 <script>
+import axios from 'axios';
+
+const token = document.getElementsByName("csrf-token")[0].getAttribute("content");
+axios.defaults.headers.common["X-CSRF-Token"] = token;
+
 export default {
   data: function () {
     return {
