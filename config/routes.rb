303c7197f6953082142e8d20b356677bc057c1aa
kazukitokunaga <tokunagakazuki330@gmail.com>
2019-01-01 15:27:03 +0900

    routes.rbの修正、before_actionの設定　等 #4

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  devise_for module: :users, controllers: {
-    sessions:      'users/sessions',
-	  passwords:     'users/passwords',
-    registrations: 'users/registrations',
-    confirmation: 'users/confirmations',
-  }
+  devise_for :users
   root to: 'page#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
