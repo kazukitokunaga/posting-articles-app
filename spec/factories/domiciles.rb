a61ccfd6978192f1990f043299f1bd59da47bd63
kazukitokunaga <tokunagakazuki330@gmail.com>
2019-01-04 00:14:56 +0900

    モデルのテスト #11

--- spec/factories/domiciles.rb
+++ spec/factories/domiciles.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :address do
+  factory :domicile, class: Address do
     postal_code   { "MyString" }
     address_kana  { "MyString" }
     sequence(:address) { |n| "住所#{n}" }
